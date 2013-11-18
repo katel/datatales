d5ca3bd09d62768d5e1e4eff38676330d46c1c63
katel <kate.lomax@gmail.com>
2013-11-18 07:33:59 +0000

    some initial custom jquery

---
+++ sites/all/themes/curator_theme/scripts/custom.js
@@ -0,0 +1,26 @@
+// custom js for curatorial
+
+(function ($) {
+  Drupal.behaviors.gridModule = {
+    attach: function (context, settings) {
+      $('.gridly').gridly({
+    base: 60, // px
+    gutter: 20, // px
+    columns: 12
+  });
+    }
+  };
+})(jQuery);
+
+
+jQuery(function(){ //DOM Ready
+
+    jQuery(".view-drag-and-drop-demo .views-row").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [140, 140]
+    });
+
+});
+
+
+
